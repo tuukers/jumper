e0dd861db03d318b4b2499168cb3b1c1353cea80
RyanFunk2000 <77073464+RyanFunk2000@users.noreply.github.com>
2022-05-22 16:52:04 -0600

    add terminal functions to UserInterface
    code taken from TerminalService in cse210-student-articulate-csharp-complete/Unit03/Game/

--- Game/Guessing/UserInterface.cs
+++ Game/Guessing/UserInterface.cs
@@ -1,1 +1,26 @@
-//collects and returns a 1 letter string from the user.
+using System;
+
+namespace Jumper
+{
+    // Handler for terminal operations.
+    // Provide input and output operations for the user interface.
+    public class UserInterface
+    {
+        public UserInterface()
+        {
+        }
+
+        // Direct user with given prompt and get one letter string
+        public void ReadLetter(string prompt)
+        {
+            Console.Write(prompt);
+            return Console.ReadLine();
+        }
+
+        // Display given text on UI 
+        public void WriteText(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+} 
