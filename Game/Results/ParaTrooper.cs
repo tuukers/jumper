29fc4bb20ca84494e188d192de68c727f61dd079
colh99 <colbyhale199@gmail.com>
2022-05-24 13:06:35 -0600

    added csproj file, .gitignore file, and fixed errors in ParaTrooper.cs

--- Game/Results/ParaTrooper.cs
+++ Game/Results/ParaTrooper.cs
@@ -10,7 +10,7 @@ namespace Jumper
         //Constructor
         public ParaTrooper()
         {
-            this.MistakeCount=0;
+            this.MistakeCount = 0;
         }
 
         //Methods
@@ -19,55 +19,55 @@ namespace Jumper
         {
             if(Wrong)
             {
-                this.MistakeCount+=1;
+                this.MistakeCount += 1;
             }
 
             if(this.MistakeCount==0)
             {
-                Console.WriteLine("""  ___""");
-                Console.WriteLine(""" /___\""");
-                Console.WriteLine(""" \   /""");
-                Console.WriteLine("""  \ /""");
-                Console.WriteLine("""   o""");
-                Console.WriteLine("""  /|\""");
-                Console.WriteLine("""  / \""");
-                Console.WriteLine(""" """);
-                Console.WriteLine("""^^^^^^^""");
+                Console.WriteLine("  ___");
+                Console.WriteLine(@" /___\");
+                Console.WriteLine(@" \   /");
+                Console.WriteLine(@"  \ /");
+                Console.WriteLine("   o");
+                Console.WriteLine(@"  /|\");
+                Console.WriteLine(@"  / \");
+                Console.WriteLine(" ");
+                Console.WriteLine("^^^^^^^");
             }
-            else if(this.MistakeCount==1)
+            else if(this.MistakeCount == 1)
             {
-                Console.WriteLine(""" /___\""");
-                Console.WriteLine(""" \   /""");
-                Console.WriteLine("""  \ /""");
-                Console.WriteLine("""   o""");
-                Console.WriteLine("""  /|\""");
-                Console.WriteLine("""  / \""");
-                Console.WriteLine(""" """);
-                Console.WriteLine("""^^^^^^^""");
+                Console.WriteLine(@" /___\");
+                Console.WriteLine(@" \   /");
+                Console.WriteLine(@"  \ /");
+                Console.WriteLine(@"   o");
+                Console.WriteLine(@"  /|\");
+                Console.WriteLine(@"  / \");
+                Console.WriteLine(" ");
+                Console.WriteLine("^^^^^^^");
             }
-            else if(this.MistakeCount==2)
+            else if(this.MistakeCount == 2)
             {
-                Console.WriteLine(""" \   /""");
-                Console.WriteLine("""  \ /""");
-                Console.WriteLine("""   o""");
-                Console.WriteLine("""  /|\""");
-                Console.WriteLine("""  / \""");
-                Console.WriteLine(""" """);
-                Console.WriteLine("""^^^^^^^""");
+                Console.WriteLine(@" \   /");
+                Console.WriteLine(@"  \ /");
+                Console.WriteLine("   o");
+                Console.WriteLine(@"  /|\");
+                Console.WriteLine(@"  / \");
+                Console.WriteLine(" ");
+                Console.WriteLine("^^^^^^^");
             }
-            else if(this.MistakeCount==3)
+            else if(this.MistakeCount == 3)
             {
-                Console.WriteLine("""  \ /""");
-                Console.WriteLine("""   o""");
-                Console.WriteLine("""  /|\""");
-                Console.WriteLine("""  / \""");
-                Console.WriteLine(""" """);
-                Console.WriteLine("""^^^^^^^""");
+                Console.WriteLine(@"  \ /");
+                Console.WriteLine("   o");
+                Console.WriteLine(@"  /|\");
+                Console.WriteLine(@"  / \");
+                Console.WriteLine(" ");
+                Console.WriteLine("^^^^^^^");
             }
-            else if(this.MistakeCount==4)
+            else if(this.MistakeCount == 4)
             {
-                Console.WriteLine("""*Censored*""");
-                Console.WriteLine("""^^^^^^^""");
+                Console.WriteLine("*Censored*");
+                Console.WriteLine("^^^^^^^");
             }
         }
     }
